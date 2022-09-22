4aeafcb7e77621f0f3bf5485a5900bd25fea207b
Quoc Anh <caoquocanhbkvn@gmail.com>
2022-09-22 14:40:49 +0700

    quocanh123 commit git test lan 2

--- src/main/java/com/example/scheduling/TestGit.java
+++ src/main/java/com/example/scheduling/TestGit.java
@@ -2,6 +2,6 @@ package com.example.scheduling;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hi");
     }
 }

0a0abc420b57840c9adf266f6729c09a025e38c5
Wapfand <89070954+Wapfand@users.noreply.github.com>
2022-09-27 09:45:30 +0700

    thay doi nay la o remote

--- src/main/java/com/example/scheduling/TestGit.java
+++ src/main/java/com/example/scheduling/TestGit.java
@@ -2,7 +2,7 @@ package com.example.scheduling;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println(" thay doi nay la o local");
+        System.out.println(" thay doi nay la o remote");
         System.out.println("hi bro");
     }
 }

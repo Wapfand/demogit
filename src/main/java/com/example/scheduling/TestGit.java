17e39b540ba72513fe6dbb34e39b4a4791d52aec
Quoc Anh <caoquocanhbkvn@gmail.com>
2022-09-27 09:42:12 +0700

    thay doi nay la o local

--- src/main/java/com/example/scheduling/TestGit.java
+++ src/main/java/com/example/scheduling/TestGit.java
@@ -2,6 +2,7 @@ package com.example.scheduling;
 
 public class TestGit {
     public static void main(String[] args) {
+        System.out.println(" thay doi nay la o local");
         System.out.println("hi bro");
     }
 }
